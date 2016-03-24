7d994489ea9268524bb8a767bac9693508e9de64
Popey Gilbert <popey@debenclipper.com>
2016-03-24 19:59:35 +0000

    some more features

--- app.js
+++ app.js
@@ -7,16 +7,17 @@ var q = require('q')
 var MongoClient = mongodb.MongoClient
 
 var saveData = {}
+var games = ["csgo"]
 
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 Date.prototype.getWeekNumber = function(){
-    var d = new Date(+this);
-    d.setHours(0,0,0);
-    d.setDate(d.getDate()+4-(d.getDay()||7));
-    return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7);
+    var d = new Date(+this)
+    d.setHours(0,0,0)
+    d.setDate(d.getDate()+4-(d.getDay()||7))
+    return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7)
 };
 
 function connectToDatabase(url, callback) {
@@ -31,19 +32,14 @@ function connectToDatabase(url, callback) {
 
 function queryServer(server, callback, iteration) {
   // console.log(server)
-  var combined = {}
   host = server.host.split(":")
   server.host = host[0]
   server.port = parseInt(host[1])
-  combined.host = server.host + ":" + server.port
-  combined.type = server.type
+  var combined = {host: server.host, port: server.port, type: server.type}
   // console.log(host)
   gamedig.query(server, function(state) {
     if (state.error) {
       logger.warning("Server is offline or inaccessible, using backup")
-      // if (iteration < 4) {
-      //   queryServer(combined, callback, iteration+1)
-      // }
       if (saveData[combined.host]) {
         callback(saveData[combined.host])
       } else {
@@ -64,6 +60,7 @@ function handleData(state, collection, callback, players, serverStats) {
     if (state.players[i].name.indexOf("Unknown from ") == -1) {
       names.push(state.players[i].name)
       addTime(state.players[i].name, state.query.address + ":" + state.query.port, players, serverStats)
+      incServer(serverStats, state, state.players[i].name)
     }
   }
 
@@ -84,6 +81,8 @@ function handleData(state, collection, callback, players, serverStats) {
 }
 
 function collectServerList(serverList, callback) {
+
+
   serverList.find({}).sort({_id: 1}).toArray(function(err, result) {
     callback(result)
   })
@@ -95,8 +94,7 @@ function addServerToList(serverList, server, callback) {
   })
 }
 
-/*
-PLAYER OBJECT {
+/* PLAYER OBJECT {
   name: playername
   servers: [
     {
@@ -107,15 +105,7 @@ PLAYER OBJECT {
       monthly: [month: timePlayed, month: timePlayed]
     }
   ]    
-}
-
-SERVER OBJECT {
-  name: servername
-  ip: serverIP
-  rank: {rankData: rankData, currentRank:currentRank}
-  players: {playerName: timePlayed, playerName: timePlayed}
-}
-*/
+} */
 
 function addTime(playername, server, players, serverStats) {
   players.find({name: playername}).toArray(function (err, result) {
@@ -131,22 +121,22 @@ function addTime(playername, server, players, serverStats) {
       currentWeek = [d.getWeekNumber(), d.getFullYear()].join("|")
       currentMonth = [d.getMonth(), d.getFullYear()].join("|")
       // logger.log(currentHour + " " + currentDay + " " + currentWeek + " " + currentMonth)
-      var serverIndex = -1
+      var index = -1
       for (i=0; i<data.servers.length; i++) {
         if (data.servers[i].name == server) {
-          serverIndex = i
+          index = i
         }
       }
-      if (serverIndex > -1) {
-        if (data.servers[serverIndex].hourly[currentHour]) { data.servers[serverIndex].hourly[currentHour] += config.refresh
-        } else { data.servers[serverIndex].hourly[currentHour] = config.refresh }
-        if (data.servers[serverIndex].daily[currentDay]) { data.servers[serverIndex].daily[currentDay] += config.refresh
-        } else { data.servers[serverIndex].daily[currentDay] = config.refresh }
-        if (data.servers[serverIndex].weekly[currentWeek]) { data.servers[serverIndex].weekly[currentWeek] += config.refresh
-        } else { data.servers[serverIndex].weekly[currentWeek] = config.refresh }
-        if (data.servers[serverIndex].monthly[currentMonth]) { data.servers[serverIndex].monthly[currentMonth] += config.refresh
-        } else { data.servers[serverIndex].monthly[currentMonth] = config.refresh }
-        data.servers[serverIndex].total += config.refresh
+      if (index > -1) {
+        if (data.servers[index].hourly[currentHour]) { data.servers[index].hourly[currentHour] += config.refresh
+        } else { data.servers[index].hourly[currentHour] = config.refresh }
+        if (data.servers[index].daily[currentDay]) { data.servers[index].daily[currentDay] += config.refresh
+        } else { data.servers[index].daily[currentDay] = config.refresh }
+        if (data.servers[index].weekly[currentWeek]) { data.servers[index].weekly[currentWeek] += config.refresh
+        } else { data.servers[index].weekly[currentWeek] = config.refresh }
+        if (data.servers[index].monthly[currentMonth]) { data.servers[index].monthly[currentMonth] += config.refresh
+        } else { data.servers[index].monthly[currentMonth] = config.refresh }
+        data.servers[index].total += config.refresh
       } else {
         data.servers.push({
           name: server,
@@ -165,28 +155,51 @@ function addTime(playername, server, players, serverStats) {
       })
     }
   })
-  /*
-  players.insert(player, function(err, result) {
-    // if (err) logger.warning(playername + " already exists")
-    if (!err) logger.log(playername + " added to database")
-    players.update({name: playername}, { $push: { rawData: {timestamp: new Date(), time: config.refresh, server: server}}}, function(err, result) {
-      if (err) logger.error(err)
-      players.update({name: playername}, { $inc: {totalTime: config.refresh} })
-      // else logger.log(result)
-    })
-  })
-  */
 }
 
-function getPlayerTimeData(playername, players, callback) {
-  players.find({name: playername}).sort({_id: 1}).toArray(function(err, result) {
-    callback(result)
+/* SERVER OBJECT {
+  name: servername
+  ip: serverIP
+  type: csgo
+  prettyType: Counter-Strike: Global Offensive
+  rank: {rankData: rankData, currentRank:currentRank}
+  players: {playerName: timePlayed, playerName: timePlayed}
+  maps: {total: 0, jb_jail: 0}
+} */
+
+function incServer(serverStats, state, playerName) {
+  console.log(state)
+  serverStats.find({ip: state.query.address + ":" + state.query.port}).toArray(function (err, result) {
+    if (err) {
+      logger.log(err);
+    } else if (result.length) {
+      // UPDATE DATA
+      var result = result[0]
+      result[name] = state.name
+      result[type] = state.query.type
+      result[prettyType] = state.query.pretty
+    } else {
+      var dataStuff = {
+        ip: state.query.host + ":" + state.query.port,
+        name: state.name,
+        type: state.query.type,
+        prettyType: state.query.pretty,
+        players: {}
+      }
+      dataStuff.players[playerName] = config.refresh
+      if (state.query.type in games) {
+        dataStuff.maps["total"] = 1
+        dataStuff.maps[state.map] = 1
+        // maps[]
+      }
+      serverStats.insert(dataStuff)
+    }
   })
 }
 
 function getPlayerTime(playername, players, callback) {
-  getPlayerTimeData(playername, players, function(value) {
-    callback(value[0])
+  players.find({name: playername}).sort({_id: 1}).toArray(function(err, result) {
+    callback(result)
   })
 }
 
@@ -198,15 +211,6 @@ connectToDatabase(config.url, function(db) {
   serverList.createIndex( { host: 1 }, { unique: true } )
   players.createIndex( { name: 1 }, { unique: true } )
 
-  /*addServerToList(serverList, {type: "teamspeak3", host: "ts.outbreak-community.com:9987"}, function(){ 
-    logger.log("One Implemented.") 
-    addServerToList(serverList, {type: "teamspeak3", host: "steam-speak.com:9987"}, function(){ 
-      logger.log("Two Implemented.") 
-      collectServerList(serverList, function(servers) {
-        logger.log(servers)
-      })
-    })
-  })*/
   // addServerToList(serverList, {type: "csgo", host: "jb.join-ob.com:27015"}, function(){})
 
   if (config.gatherData) {
@@ -215,6 +219,7 @@ connectToDatabase(config.url, function(db) {
         for (j = 0; j < servers.length; j++) {
           queryServer(servers[j], function(state) {
             handleData(state, data, function() {}, players, serverStats)
+            incServer(serverStats, state)
           }, 0)
         }
       })
